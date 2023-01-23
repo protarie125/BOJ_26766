d2e10227361bba928b2f0d879791d36f1700dcd9
protarie125 <protarie@gmail.com>
2023-01-23 17:40:49 +0900

    초기 커밋.

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+void p(ostringstream& oss) {
+	oss << R"( @@@   @@@ 
+@   @ @   @
+@    @    @
+@         @
+ @       @ 
+  @     @  
+   @   @   
+    @ @    
+     @     )";
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	auto oss = ostringstream{};
+	while (0 < (n--)) {
+		p(oss);
+		if (0 < n) {
+			oss << '\n';
+		}
+	}
+
+	cout << oss.str();
+
+	return 0;
+}
